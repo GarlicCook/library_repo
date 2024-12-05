e7ffcf9148cd1d7c99bf3c9779e2570c45b0202b
jaesung05 <92916116+jaesung05@users.noreply.github.com>
2024-12-05 15:52:24 +0900

    Add files via upload

---
+++ ImagePreprocessor.py
@@ -0,0 +1,14 @@
+class ImagePreprocessor():
+    def __init__(self):
+        pass
+    def feat1(self):
+        pass
+    def feat2(self):
+        pass
+    def feat3(self):
+        pass
+    def feat4(self):
+        pass
+    def feat5(self):
+        pass
+    
