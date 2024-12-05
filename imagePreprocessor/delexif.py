b20da40480e74f4640b5660440002beb8cdf87a2
YOO JIYO <rss1234567@hanyang.ac.kr>
2024-12-06 00:55:11 +0900

    delexif bug fix

--- imagePreprocessor/delexif.py
+++ imagePreprocessor/delexif.py
@@ -7,9 +7,9 @@ def modexif(image_path, output_path, make = b"", model = b"", datetime = b""):
     #이미지 메타데이터 읽기
 
 
-    exif_dict['0th'][271] = (make if make == b"" else make.decode("utf-8"))
+    exif_dict['0th'][271] = (make if make == b"" else make.encode("utf-8"))
     #제조사 삭제 or 수정
-    exif_dict['0th'][272] = (model if model == b"" else model.decode("utf-8"))
+    exif_dict['0th'][272] = (model if model == b"" else model.encode("utf-8"))
     #모델이름 삭제 or 수정
 
 
@@ -22,8 +22,8 @@ def modexif(image_path, output_path, make = b"", model = b"", datetime = b""):
 
 
 
-    exif_dict['Exif'][36867] = (datetime if datetime == b"" else datetime.decode("utf-8"))
-    exif_dict['Exif'][36868] = (datetime if datetime == b"" else datetime.decode("utf-8"))
+    exif_dict['Exif'][36867] = (datetime if datetime == b"" else datetime.encode("utf-8"))
+    exif_dict['Exif'][36868] = (datetime if datetime == b"" else datetime.encode("utf-8"))
     #찍은시간 삭제 or 수정
 
 
