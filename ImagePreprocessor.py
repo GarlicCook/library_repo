9a2ac6d5e5d3ca4a03e1677710e453b499b20abd
jaesung05 <92916116+jaesung05@users.noreply.github.com>
2024-12-05 13:58:02 +0900

    Update ImagePreprocessor.py

--- ImagePreprocessor.py
+++ ImagePreprocessor.py
@@ -83,18 +83,18 @@ class ImagePreprocessor():
     """
     여기다가 각자 코드 추가하셔서 merge하시면 될 것 같습니다.
     """
-    def count_faces(image_path):
+    def count_faces(self,image_path):
         # Haar Cascade 모델 로드
         face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
         
         # 이미지 로드
-        image = cv2.imread(image_path)
+        image = cv2.imread(self.image_path)
         if image is None:
             print("이미지를 로드할 수 없습니다. 경로를 확인하세요.")
             return 0
     
         # 그레이스케일 변환 (얼굴 탐지에 효과적)
-        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        gray_image = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
     
         # 얼굴 탐지
         faces = face_cascade.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
@@ -104,10 +104,10 @@ class ImagePreprocessor():
     
         # 탐지된 얼굴에 사각형 그리기
         for (x, y, w, h) in faces:
-            cv2.rectangle(image, (x, y), (x+w, y+h), (255, 0, 0), 2)
+            cv2.rectangle(self.image, (x, y), (x+w, y+h), (255, 0, 0), 2)
     
         # 결과 이미지 표시
-        cv2.imshow('Detected Faces', image)
+        cv2.imshow('Detected Faces', self.image)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
     
