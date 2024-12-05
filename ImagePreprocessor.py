643f181284605d999a501b1c697f59acf5e9173f
jaesung05 <92916116+jaesung05@users.noreply.github.com>
2024-12-05 15:56:15 +0900

    Update ImagePreprocessor.py

--- ImagePreprocessor.py
+++ ImagePreprocessor.py
@@ -1,6 +1,19 @@
 import cv2
 
 class ImagePreprocessor():
+    def __init__(self):
+        pass
+    def feat1(self):
+        pass
+    def feat2(self):
+        pass
+    def feat3(self):
+        pass
+    def feat4(self):
+        pass
+    def feat5(self):
+        pass
+        
     def __init__(self, image_path=None):
         self.image_path = image_path  # 이미지 경로 초기화
 
